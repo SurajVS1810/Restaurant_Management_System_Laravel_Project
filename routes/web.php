81914a0343c1506a424edd37c77a153ca08b26e7
Suraj <surajsubrahmanyn@gmail.com>
2023-03-13 00:05:52 +0530

    commit

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/home',[HomeController::class,"index"] );
+
+Route::get('/',[HomeController::class,"index"] );
 
 Route::middleware([
     'auth:sanctum',
