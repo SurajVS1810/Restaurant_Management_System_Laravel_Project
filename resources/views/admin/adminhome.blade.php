fbcae5398dd57f1c38c8a69551c3aad44a15d665
Suraj <surajsubrahmanyn@gmail.com>
2023-03-14 08:56:44 +0530

    insert data into db

--- resources/views/admin/adminhome.blade.php
+++ resources/views/admin/adminhome.blade.php
@@ -9,7 +9,11 @@
   </head>
   <body>
    
+  <div class="container-scroller">
+
   @include("admin.navbar")
+
+</div>
   @include("admin.adminscript") 
   </body>
 </html>
---
+++ resources/views/admin/foodmenu.blade.php
@@ -0,0 +1,52 @@
+<x-app-layout>
+
+</x-app-layout>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include("admin.admincss")
+  </head>
+  <body>
+  <div class="container-scroller">
+
+ 
+  @include("admin.navbar")
+  <div style="position: relative;top:60px; right: -150px">
+
+  <form action="{{('/uploadfood')}}" method="post" enctype="multipart/form-data">
+    @csrf
+    <div>
+        <label>Title</label>
+        <input style="color:blue;" type="text" name="title" placeholder="write a title" required>
+</div>
+
+<div>
+        <label>Price</label>
+        <input style="color:blue;" type="num" name="price" placeholder="price" required>
+</div>
+
+<div>
+        <label>Image</label>
+        <input  type="file" name="image"  required>
+</div>
+
+<div>
+        <label>Description</label>
+        <input style="color:blue;" type="text" name="description" placeholder="Description"  required>
+</div>
+
+<div>
+   
+        <input  type="submit" value="Save">
+</div>
+
+</form>
+
+</div>
+
+</div>
+
+  @include("admin.adminscript") 
+  </body>
+</html>
